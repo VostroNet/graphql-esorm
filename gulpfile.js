b9c09d9ae24a9943da344a571b3e2edc5bafa620
Matthew Mckenzie <mmckenzie@vostronet.com>
2019-05-07 18:47:19 +1000

    upgrading gulp

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ gulp.task("compile:publish", gulp.series("lint", () => {
         [
           "@babel/preset-env", {
             "targets": {
-              "node": "10.14.1",
+              "node": "10.15.3",
             },
             "useBuiltIns": "usage",
           },
